95b2647c826155fe7abff6b5f0fc877d9354e6f1
touzai <narumitouzai@gmail.com>
2025-02-16 22:39:35 +0900

    ogpの仕組みを調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,38 +2,25 @@ import "./styles.css";
 
 export const metadata = {
   title: "三目並べ",
-  description: "Reactチュートリアルの三目並べ",
-  icons: {
-    icon: "/assets/icon.ico",
-  },
+  description:
+  "Reactチュートリアルの三目並べ",
+  icons: "/assets/icon.ico",
+
   openGraph: {
-    type: "website",
     title: "三目並べ",
-    description: "Reactチュートリアルの三目並べ",
-    siteName: "三目並べ！",
-    url: "https://narumitouzai.github.io/ReactTutorial/",
-    images: {
+    description:
+      "Reactチュートリアルの三目並べ",
+    images: [{
       url: "/assets/ogp.png",
-      type: "image/png",
       width: 1200,
-      height: 630,
-    },
+      height: 630
+    }],
   },
   twitter: {
-    type: "website",
-    title: "三目並べ",
-    description: "Reactチュートリアルの三目並べ",
-    siteName: "三目並べ！",
-    url: "https://narumitouzai.github.io/ReactTutorial/",
-    images: {
-      url: "/assets/ogp.png",
-      type: "image/png",
-      width: 1200,
-      height: 630,
-    },
-    card: "summary",
+    card: "summary_large_image",
+    images: ["/assets/ogp.png"]
   },
-}
+};
 
 export default function RootLayout({
   children,
